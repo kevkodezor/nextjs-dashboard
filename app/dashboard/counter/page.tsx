9a9382b09b2c79d7ef3a3f68b688998d98b3c1e9
Kevin <keviinnalvarez2015@gmail.com>
2023-12-15 18:14:29 -0400

    estructure dashboard

---
+++ app/dashboard/counter/page.tsx
@@ -0,0 +1,8 @@
+
+export default function CounterPage () {
+    return (
+        <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+            Counter
+        </div>
+    )
+}
