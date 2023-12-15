f87fa4edcd00bac235ddefc6efbf82bdc06aab58
Kevin <keviinnalvarez2015@gmail.com>
2023-12-15 18:31:02 -0400

    sidebar & main content

--- app/dashboard/counter/page.tsx
+++ app/dashboard/counter/page.tsx
@@ -1,7 +1,7 @@
 
 export default function CounterPage () {
     return (
-        <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <div className='flex min-h-screen flex-col items-center justify-between p-10'>
             Counter
         </div>
     )
