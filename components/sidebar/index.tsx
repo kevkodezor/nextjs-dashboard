ea1125c53a2a07377195f3885ba49e13f7483acf
Kevin <keviinnalvarez2015@gmail.com>
2023-12-15 18:44:45 -0400

    enable access image outsite

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 
 export const SideBar = () => {
   return (
@@ -6,19 +7,25 @@ export const SideBar = () => {
         className='bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll'
         style={{ width: '300px' }}
     >
-          <div id="logo" className="my-4 px-6">
-              <h1 className="text-lg md:text-2xl font-bold text-white">Dash<span className="text-blue-500">8</span>.</h1>
-              <p className="text-slate-500 text-sm">Manage your actions and activities</p>
+          <div id='logo' className='my-4 px-6'>
+              <h1 className='text-lg md:text-2xl font-bold text-white'>Dash<span className='text-blue-500'>8</span>.</h1>
+              <p className='text-slate-500 text-sm'>Manage your actions and activities</p>
           </div>
 
-          <div id="profile" className="px-6 py-10">
-              <p className="text-slate-500">Welcome back,</p>
-              <a href="#" className="inline-flex space-x-2 items-center">
+          <div id='profile' className='px-6 py-10'>
+              <p className='text-slate-500'>Welcome back</p>
+              <a href='#' className='inline-flex space-x-2 items-center'>
                   <span>
-                      <img className="rounded-full w-8 h-8" src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=128&q=80" alt="" />
+                    <Image 
+                        className='rounded-full w-8 h-8'
+                        src='https://images.unsplash.com/photo-1542909168-82c3e7fdca5c'
+                        alt='img dashboard'
+                        width={10}
+                        height={10}
+                    />
                   </span>
-                  <span className="text-sm md:text-base font-bold">
-                      Edward Tompson
+                  <span className='text-sm md:text-base font-bold'>
+                    Kevin Torres
                   </span>
               </a>
           </div>
