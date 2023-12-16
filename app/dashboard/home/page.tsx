bc56e9dd43801eaa12e1144def0064c7b04813db
Kevin <keviinnalvarez2015@gmail.com>
2023-12-15 20:58:57 -0400

    icons & active routes

---
+++ app/dashboard/home/page.tsx
@@ -0,0 +1,8 @@
+
+export default function HomePage() {
+    return (
+        <div className='flex min-h-screen flex-col items-center justify-between p-10'>
+            Home
+        </div>
+    )
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import { Metadata } from 'next'
 import { SideBar } from '@/components'
 
 export const metadata: Metadata = {
