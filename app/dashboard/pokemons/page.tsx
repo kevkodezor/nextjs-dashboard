fe8f249ede00cbdcd20554e64814edc58ae0a430
Kevin <keviinnalvarez2015@gmail.com>
2023-12-18 19:23:46 -0400

    render name pokemons

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -1,8 +1,18 @@
 
-export default function PokemonsPage () {
+const getPokemons = async (limit = 20, offset = 0) => {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`)
+    .then(res => res.json());
+    return data;    
+}
+
+export default async function PokemonsPage () {
+    const { results } = await getPokemons();
+    
     return (
-        <div>
-            pokemons
+        <div className='flex flex-col w-full'>
+            {results.map((pokemon:any) => (
+                <text key={pokemon.name}>{pokemon.name}</text>
+            ))}
         </div>
     );
 }
