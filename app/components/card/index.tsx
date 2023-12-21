6f5eec8784db88f9e30059364dd97caf2149c2c3
Kevin <keviinnalvarez2015@gmail.com>
2023-12-21 16:38:47 -0400

    corrections styles

--- app/components/card/index.tsx
+++ app/components/card/index.tsx
@@ -11,11 +11,11 @@ export const Card = ({ pokemon }: Props) => {
     return (
         <div className='mx-auto right-0 mt-2 w-60'>
             <div className='flex flex-col bg-white rounded overflow-hidden shadow-lg'>
-                <div className='flex flex-col items-center justify-center text-center p-6 bg-gray-800 border-b'>
+                <div className='flex flex-col items-center justify-center text-center p-6 bg-gray-800 border-b h-80'>
                     <Image
                         key={pokemon.id}
                         alt={pokemon.name}
-                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
                         width={100}
                         height={100}
                         priority={false}
@@ -30,20 +30,17 @@ export const Card = ({ pokemon }: Props) => {
                         </Link>
                     </div>
                 </div>
-                <div className='border-b'>
-                    <Link href='#' className='px-4 py-2 hover:bg-gray-100 flex items-center'>
-                        <div className='text-red-600'>
-                            <IoHeartOutline />
-                        </div>
-                        <div className='pl-3'>
-                            <p className='text-sm font-medium text-gray-800 leading-none'>
-                                Favorite
-                            </p>
-                            <p className='text-xs text-gray-500'>View in favorites</p>
-                        </div>
-                    </Link>
-
-                </div>
+                <Link href='#' className='px-4 py-2 hover:bg-gray-200 flex items-center'>
+                    <div className='text-red-600'>
+                        <IoHeartOutline />
+                    </div>
+                    <div className='pl-3'>
+                        <p className='text-sm font-medium text-gray-800 leading-none'>
+                            Favorite
+                        </p>
+                        <p className='text-xs text-gray-500'>View in favorites</p>
+                    </div>
+                </Link>
             </div>
         </div>
     )
